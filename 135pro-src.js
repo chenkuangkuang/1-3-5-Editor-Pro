e5bb20b527bc944839cc909e6c81bad5f68ebebd
chenkuangkuang <540342678@qq.com>
2023-10-25 10:24:50 +0800

    test email notification 3

--- 135pro-src.js
+++ 135pro-src.js
@@ -7,13 +7,13 @@ var timer = setInterval(() => {
   times++;
   var con = document.querySelector("#editor-template");
   if (con) {
-      // 左上角面板点击，重新绑定生成悬浮复制按钮
-      $("#style-categories").on("click", function(e){
-          console.log('面板被点击22',);
-          setTimeout(() => {
-              bindCopyBtn();
-          }, 1000);
-      })
+    // 左上角面板点击，重新绑定生成悬浮复制按钮
+    $("#style-categories").on("click", function(e){
+        console.log('面板被点击22',);
+        setTimeout(() => {
+            bindCopyBtn();
+        }, 1000);
+    })
     clearInterval(timer);
     console.log('发现dom editor-template');
     var mask = document.querySelector(".g-view-mask");
